70b6b2e4bdd6de64d1be3ca2116d0d106b4ac814
Irene Runtu <diana.pri1223@gmail.com>
2022-09-23 02:42:31 +0700

    Coding Calculator

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the action to be performed");
+Console.WriteLine("Press 1 for Addition");
+Console.WriteLine("Press 2 for Substraction");
+Console.WriteLine("Press 3 for Multiplication");
+Console.WriteLine("Press 4 for Division");
+
+Console.WriteLine();
+int inputPengguna = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 1st input: ");
+int angka1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 2st input: ");
+int angka2 = int.Parse(Console.ReadLine());
+
+switch (inputPengguna)
+{
+    case 1:
+    int penjumlahan = angka1 + angka2;
+    Console.WriteLine(penjumlahan);
+    break;
+
+    case 2:
+    int pengurangan = angka1 - angka2;
+    Console.WriteLine(pengurangan);
+    break;
+
+    case 3:
+    int perkalian = angka1 * angka2;
+    Console.WriteLine(perkalian);
+    break;
+
+    case 4:
+    double pembagian = (double) angka1 / (double) angka2;
+    Console.WriteLine(pembagian);
+    break;
+}
